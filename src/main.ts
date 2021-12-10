8c2fc9184c402617be836a971f74221aa6d01261
Philipp Lätsch <p.laetsch@outlook.de>
2021-12-10 22:23:58 +0100

    position sync

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,18 @@ const io = require('socket.io')(3001,  {
 
 console.log("Server starting ...")
 
+
 io.on("connection", (socket: Socket) => {
     console.log("New connection!");
+    socket.broadcast.emit('playerConnect', socket.id)
+
+    socket.on('localPlayerLocationUpdate', (pos, dir) => {
+      //console.log('new location package', pos, dir)
+      socket.broadcast.emit('playerLocationUpdate', socket.id, pos, dir)
+    })
+
+    socket.on('disconnect', (reason: string) => {
+      socket.broadcast.emit('playerDisconnect', socket.id)
+    })
 });
 
